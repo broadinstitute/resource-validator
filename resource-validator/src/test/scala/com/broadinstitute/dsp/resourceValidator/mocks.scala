ba12835a848312e9cb8a68715249e2df707eb332
Qi Wang <qi@broadinstitute.org>
2020-09-17 16:52:28 -0400

    compile it

--- resource-validator/src/test/scala/com/broadinstitute/dsp/resourceValidator/mocks.scala
+++ resource-validator/src/test/scala/com/broadinstitute/dsp/resourceValidator/mocks.scala
@@ -3,6 +3,7 @@ package resourceValidator
 
 import fs2.Stream
 import cats.effect.IO
+import org.broadinstitute.dsde.workbench.google2.GKEModels
 
 class FakeDbReader extends DbReader[IO] {
   override def getDeletedRuntimes: Stream[IO, Runtime] = Stream.empty
@@ -10,4 +11,6 @@ class FakeDbReader extends DbReader[IO] {
   override def getErroredRuntimes: Stream[IO, Runtime] = Stream.empty
 
   override def getBucketsToDelete: Stream[IO, BucketToRemove] = Stream.empty
+
+  override def getK8sClustersToDelete: Stream[IO, GKEModels.KubernetesClusterId] = Stream.empty
 }
