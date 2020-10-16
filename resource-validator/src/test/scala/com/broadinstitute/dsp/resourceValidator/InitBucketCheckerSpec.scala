9375b60aa1ce8e3f615ac997222664edd39596ec
jdcanas <jcanas@broadinstitute.com>
2020-10-16 14:00:02 -0400

    fmt

--- resource-validator/src/test/scala/com/broadinstitute/dsp/resourceValidator/InitBucketCheckerSpec.scala
+++ resource-validator/src/test/scala/com/broadinstitute/dsp/resourceValidator/InitBucketCheckerSpec.scala
@@ -1,7 +1,6 @@
 package com.broadinstitute.dsp
 package resourceValidator
 
-
 import cats.effect.IO
 import com.broadinstitute.dsp.Generators._
 import com.google.cloud.storage.{Bucket, Storage}
@@ -15,49 +14,62 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.mockito.MockitoSugar
 
 class InitBucketCheckerSpec extends AnyFlatSpec with CronJobsTestSuite with MockitoSugar {
-    val mockBucket = mock[Bucket]
+  val mockBucket = mock[Bucket]
 
-    it should "return None if bucket no longer exists in Google" in {
-      val storageService = new BaseFakeGoogleStorage {
-        override def getBucket(googleProject: GoogleProject, bucketName: GcsBucketName, bucketGetOptions: List[Storage.BucketGetOption], traceId: Option[TraceId]): IO[Option[Bucket]] =
-          IO.pure(None)
+  it should "return None if bucket no longer exists in Google" in {
+    val storageService = new BaseFakeGoogleStorage {
+      override def getBucket(googleProject: GoogleProject,
+                             bucketName: GcsBucketName,
+                             bucketGetOptions: List[Storage.BucketGetOption],
+                             traceId: Option[TraceId]): IO[Option[Bucket]] =
+        IO.pure(None)
+    }
+    val checkerDeps =
+      initBucketCheckerDeps(storageService)
+
+    forAll { (bucket: InitBucketToRemove, dryRun: Boolean) =>
+      val dbReader = new FakeDbReader {
+        override def getInitBucketsToDelete: Stream[IO, InitBucketToRemove] = Stream.emit(bucket)
       }
+      val checker = InitBucketChecker.impl(dbReader, checkerDeps)
+      val res = checker.checkResource(bucket, dryRun)
+      res.unsafeRunSync() shouldBe None
+    }
+  }
+
+  it should "return bucket if bucket still exists in Google" in {
+    forAll { (bucket: InitBucketToRemove, dryRun: Boolean) =>
+      val storageService = new BaseFakeGoogleStorage {
+        override def getBucket(googleProject: GoogleProject,
+                               bucketName: GcsBucketName,
+                               bucketGetOptions: List[Storage.BucketGetOption],
+                               traceId: Option[TraceId]): IO[Option[Bucket]] =
+          IO.pure(Some(mockBucket))
+
+        override def deleteBucket(googleProject: GoogleProject,
+                                  bucketName: GcsBucketName,
+                                  isRecursive: Boolean,
+                                  bucketSourceOptions: List[Storage.BucketSourceOption],
+                                  traceId: Option[TraceId],
+                                  retryConfig: RetryConfig): Stream[IO, Boolean] =
+          if (dryRun) Stream.emit(fail("this shouldn't be called")) else Stream.emit(true).covary[IO]
+      }
+
+      val dbReader = new FakeDbReader {
+        override def getInitBucketsToDelete: Stream[IO, InitBucketToRemove] = Stream.emit(bucket)
+      }
+
       val checkerDeps =
         initBucketCheckerDeps(storageService)
-
-      forAll { (bucket: InitBucketToRemove, dryRun: Boolean) =>
-        val dbReader = new FakeDbReader {
-          override def getInitBucketsToDelete: Stream[IO, InitBucketToRemove] = Stream.emit(bucket)
-        }
-        val checker = InitBucketChecker.impl(dbReader, checkerDeps)
-        val res = checker.checkResource(bucket, dryRun)
-        res.unsafeRunSync() shouldBe None
-      }
+      val checker = InitBucketChecker.impl(dbReader, checkerDeps)
+      val res = checker.checkResource(bucket, dryRun)
+      res.unsafeRunSync() shouldBe Some(bucket)
     }
+  }
 
-    it should "return bucket if bucket still exists in Google" in {
-      forAll { (bucket: InitBucketToRemove, dryRun: Boolean) =>
-        val storageService = new BaseFakeGoogleStorage {
-          override def getBucket(googleProject: GoogleProject, bucketName: GcsBucketName, bucketGetOptions: List[Storage.BucketGetOption], traceId: Option[TraceId]): IO[Option[Bucket]] =
-            IO.pure(Some(mockBucket))
-
-          override def deleteBucket(googleProject: GoogleProject, bucketName: GcsBucketName, isRecursive: Boolean, bucketSourceOptions: List[Storage.BucketSourceOption], traceId: Option[TraceId], retryConfig: RetryConfig): Stream[IO, Boolean] =
-            if (dryRun) Stream.emit(fail("this shouldn't be called")) else Stream.emit(true).covary[IO]
-        }
-
-        val dbReader = new FakeDbReader {
-          override def getInitBucketsToDelete: Stream[IO, InitBucketToRemove] = Stream.emit(bucket)
-        }
-
-        val checkerDeps =
-          initBucketCheckerDeps(storageService)
-        val checker = InitBucketChecker.impl(dbReader, checkerDeps)
-        val res = checker.checkResource(bucket, dryRun)
-        res.unsafeRunSync() shouldBe Some(bucket)
-      }
-    }
-
-    def initBucketCheckerDeps(googleDiskService: GoogleStorageService[IO] = FakeGoogleStorageInterpreter): CheckRunnerDeps[IO] =
-      CheckRunnerDeps(config.reportDestinationBucket, googleDiskService)
+  def initBucketCheckerDeps(
+    googleDiskService: GoogleStorageService[IO] = FakeGoogleStorageInterpreter
+  ): CheckRunnerDeps[IO] =
+    CheckRunnerDeps(config.reportDestinationBucket, googleDiskService)
 
 }
