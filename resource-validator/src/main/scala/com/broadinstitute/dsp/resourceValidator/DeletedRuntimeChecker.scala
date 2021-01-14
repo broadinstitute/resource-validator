6a218a0715089a41ab3ed879522cafd09c490335
jdcanas <jcanas@broadinstitute.com>
2021-01-14 10:30:02 -0500

    scalafmt

--- resource-validator/src/main/scala/com/broadinstitute/dsp/resourceValidator/DeletedRuntimeChecker.scala
+++ resource-validator/src/main/scala/com/broadinstitute/dsp/resourceValidator/DeletedRuntimeChecker.scala
@@ -56,7 +56,9 @@ object DeletedRuntimeChecker {
                       .as(Option(runtime))
                   case false =>
                     logger
-                      .info(s"$runtime has been reported from getCluster, but billing is disabled so cannot perform any actions.")
+                      .info(
+                        s"$runtime has been reported from getCluster, but billing is disabled so cannot perform any actions."
+                      )
                       .as(none[Runtime])
                 }
             } yield r
